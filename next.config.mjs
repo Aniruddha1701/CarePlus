3752d96013b1de532c8654f25295e65c162d43ea
Aniruddha1701 <95077620+Aniruddha1701@users.noreply.github.com>
2024-07-21 21:38:51 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -15,7 +15,7 @@ export default withSentryConfig(nextConfig, {
   // https://github.com/getsentry/sentry-webpack-plugin#options
 
   org: "anyy-js",
-  project: "care-pulse",
+  project: "javascript-nextjs",
 
   // Only print logs for uploading source maps in CI
   silent: !process.env.CI,
